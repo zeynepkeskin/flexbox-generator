2f2502257ec58a339aaf8d52de0b193627b188c1
Zeynep Keskin <zeynepkeskin10@gmail.com>
2024-12-01 11:43:16 -0800

    first commit

---
+++ script.js
@@ -0,0 +1,45 @@
+let containerAttributes = {
+  display: ["flex", "inline-flex"],
+  flexDirection: ["row", "row-reverse", "column", "column-reverse"],
+  flexWrap: ["nowrap", "wrap", "wrap-reverse"],
+  justifyContent: [
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+    "space-evenly",
+  ],
+  alignItems: ["stretch", "flex-start", "flex-end", "center", "baseline"],
+  alignContent: [
+    "stretch",
+    "flex-start",
+    "flex-end",
+    "center",
+    "space-between",
+    "space-around",
+  ],
+};
+let itemAttributes = {
+  order: 0,
+  flexGrow: 0,
+  flexShrink: 1,
+  flexBasis: "auto",
+  alignSelf: "auto",
+};
+
+function createUI() {
+  let html = "";
+  for (let attr in containerAttributes) {
+    html += `${attr}: <select id="${attr}" onChange="valueChanged(this)">`;
+    for (let value of containerAttributes[attr]) {
+      html += `<option>${value}</option>`;
+    }
+    html += "</select><br>";
+  }
+  document.querySelector("#left").innerHTML = html;
+}
+createUI();
+function valueChanged(src) {
+  document.querySelector(".container").style[src.id] = src.value;
+}
